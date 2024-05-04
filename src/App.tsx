fef806835f5e62583579653c4acdac8fa36dd046
tanim0 <nyokki0731@gmail.com>
2024-05-04 18:44:00 +0900

    イベントとfunctionに型をつける②

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,38 @@ import Form from "./components/Form"
 
 const App = () => {
   const [mealName, setMealName] = useState("") // <useStateの型情報>
+  const [mealData, setMealData] = useState({ // getMealDataで取得したデータを保管するstate
+    id: "",
+    name: "",
+    instructions: "",
+    img: "",
+    source: "",
+    area: "",
+    category: "",
+  })
+
+  const getMealData = async(e: React.FormEvent<HTMLFormElement>) => { // formとonSubmitに由来するイベントなのでReact.FormEventになる ＋ <>で具体的なHTMLタグ要素を指定(HTMLFormElement)
+    e.preventDefault()
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`)
+    const jsonData = await response.json()
+    const {idMeal, strMeal, strInstructions, strMealThumb, strYoutube, strArea, strCategory} = jsonData.meals[0] // 分割代入
+
+    setMealData({
+      id: idMeal,
+      name: strMeal,
+      instructions: strInstructions,
+      img: strMealThumb,
+      source: strYoutube,
+      area: strArea,
+      category: strCategory,
+    })
+  }
+
   return (
     <div>
       <Header/>
       <h1>こんにちは</h1>
-      <Form setMealName={setMealName}/>
+      <Form setMealName={setMealName} getMealData={getMealData}/>
     </div>
   )
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,11 @@
 type FormProps = {
-  setMealName: React.Dispatch<React.SetStateAction<string>>;
+  setMealName: React.Dispatch<React.SetStateAction<string>>; // useStateを使用しているのでreactが型推測できる
+  getMealData: (e: React.FormEvent<HTMLFormElement>) => Promise<void>;  // ユーザーが独自に定義したjsの関数 //React.FormEvent<HTMLFormElement>の部分はgetMealDataで引数にe入ってくるため指定
 }
 
-const Form = ({ setMealName }: FormProps) => {
+const Form = ({ setMealName, getMealData }: FormProps) => {
   return (
-    <form>
+    <form onSubmit={getMealData}>
       <input onChange={(e) => setMealName(e.target.value)} type="text" name="mealName" placeholder="料理名を英語で入力" required />
       <button>検索</button>
     </form>
